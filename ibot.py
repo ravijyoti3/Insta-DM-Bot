8c79960a5ac6825098827eb9d00001b66c1b7a5c
sadbro <dasprabaha23@icloud.com>
2022-02-28 21:04:48 +0530

    Added continuation

--- ibot.py
+++ ibot.py
@@ -2,11 +2,12 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
-import time
-from selenium.webdriver.common.action_chains import ActionChains
-import re
-import sys
 import instaloader as ig
+
+import datetime as dt
+import time
+import sys
+import random
 
 class CSVH:
 
@@ -14,7 +15,12 @@ class CSVH:
 
     def __init__(self, fp, mode = 'r'):
 
-        self.handle = open(fp, mode)
+        try:
+            self.handle = open(fp, mode)
+
+        except:
+            print("[SYS] Vooh, Couldn't open the file.")
+
         self.mode = mode
         self.file_path = fp
 
@@ -24,7 +30,7 @@ class CSVH:
 
         if self.handle.closed:
 
-            print("<--! File is closed !-->")
+            print("[SYS] File is closed.")
             sys.exit(1)
 
         self.raw_data = self.handle.readlines()
@@ -42,29 +48,41 @@ class CSVH:
 
                 tmp_dict[key] = val
 
-            self.data[count] = count
+            self.data[count] = tmp_dict
             count += 1
 
-
-        print("<--! {} Items Loaded !-->".format(count))
+        print("[INFO] {} Items Loaded".format(count))
 
     def add(self, val):
 
-        if self.handle.closed or 'a' not in self.mode:
-
-            print("<--! File is closed or/and Wrong Mode !-->")
-            sys.exit(1)
-
-        raw_val = ",".join(val) +"\n"
-
+        raw_val = ",".join([str(i) for i in val]) +"\n"
         self.handle.write(raw_val)
+
+    def get_last(self):
+
+        if self.data:
+            pass
+
+        else:
+            self.load()
+
+        return self.data[-1]
+
+    def __del__(self):
+
+        self.handle.close()
+
+    def close(self):
+
+        self.__del__()
+        print("[SYS] File Handler for FILE({}) with MODE({}) successfully closed".format(self.file_path, self.mode))
 
 L = ig.Instaloader()
 
-username = "sadbrotech"
-password = "123456qwerty"
+uname = "rgntech"
+pword = "thisismyroot"
 
-L.login(username, password)  # (login)
+L.login(uname, pword)  # (login)
 
 profile = ig.Profile.from_username(L.context, 'monikonw')
 
@@ -78,7 +96,7 @@ mobile_emulation = { "deviceName": "iPhone 5/SE" }
 chrome_options.add_experimental_option("mobileEmulation", mobile_emulation)
 chrome_options.binary_location = r"/bin/google-chrome"
 
-browser = webdriver.Chrome("./chromedriver_linux", options=chrome_options)
+browser = webdriver.Chrome("drivers/chromedriver", options=chrome_options)
 
 try:
 
@@ -89,31 +107,28 @@ try:
     usrname_bar = browser.find_element_by_name('username')
     passwrd_bar = browser.find_element_by_name('password')
 
-    username = 'sadbrotech'  # Enter your username here
-    password = '123456qwerty'  # Enter your password here
+    username = uname  # Enter your username here
+    password = pword  # Enter your password here
 
     usrname_bar.send_keys(username)
     passwrd_bar.send_keys(password + Keys.ENTER)
 
-    print("Successfully Logged in !")
+    print("[INFO] Successfully Logged in !")
 
     time.sleep(11)
 
 except:
 
-    print("Unsuccessful Login !")
+    print("[INFO] Unsuccessful Login !")
 
 follow_list = []
-
-for follower in profile.get_followers():
-    follow_list.append(follower.username)
 
 def send_msg(usrname):
 
     try:
         browser.get('https://www.instagram.com/direct/new/')
 
-        time.sleep(5)
+        time.sleep(random.randint(1, 6)) # og 5
 
         to_btn = browser.find_elements_by_css_selector('.j_2Hd.uMkC7.M5V28')
         to_btn[0].send_keys(usrname)
@@ -124,35 +139,55 @@ def send_msg(usrname):
                 c.click()
                 break
 
-        time.sleep(3)
+        time.sleep(random.randint(1, 4)) # og 3
 
         nxt_btn = browser.find_elements_by_css_selector('.sqdOP.yWX7d.y3zKF.cB_4K')
         nxt_btn[0].click()
 
-        time.sleep(6)
+        time.sleep(random.randint(1, 7)) # og 7
 
         txt_box = browser.find_element_by_tag_name('textarea')
         txt_box.send_keys(f"Hi @{usrname} ! What's up ?")  # Customize your message
 
-        time.sleep(2)
+        time.sleep(random.randint(1, 3)) # og 2
 
         snd_btn = browser.find_elements_by_css_selector('.sqdOP.yWX7d.y3zKF')
         snd_btnn = snd_btn[len(snd_btn)-1]
         snd_btnn.click()
 
-        print("Sent Msg to {}".format(usrname))  # LOG TO CONSOLE
+        print("[INFO] Sent Msg to {}".format(usrname))  # LOG TO CONSOLE
 
-        time.sleep(4)
+        time.sleep(random.randint(1, 5)) # og 4
 
     except IndexError:
-        print('failed')
+        print('[SYS] DM failed')
 
 print(follow_list)  #--> To get the list
 
-print("\nNo of followers: {}".format(len(follow_list)))
+print("\n[INFO] No of followers: {}\n".format(len(follow_list)))
 
-for un in follow_list:
+fio = CSVH("follower.csv", "w")
 
-    send_msg(un)
+try:
 
-browser.quit()
+    fio.load()
+
+    start_index = len(fio.data)
+
+    for i in range(start_index, len(follow_list)):
+
+        send_msg(follow_list[i])
+        fio.add([i, follow_list[i], dt.datetime.now(), True])
+
+except:
+
+    fio.add(["id", "ig_handle", "time", "sent"])
+
+    for i, un in enumerate(follow_list):
+
+        send_msg(un)
+        fio.add([i, un, dt.datetime.now(), True])
+
+fio.close()
+
+#browser.quit()
